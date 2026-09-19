a85e2e73f6306e8fcb1aad239e61aa17fe28ed00
Radhika Kajale <radhikakajale96@gmail.com>
2026-09-19 22:44:50 +0530

    fix(deploy): automatically run migrate on wsgi startup

--- server/config/wsgi.py
+++ server/config/wsgi.py
@@ -1,5 +1,13 @@
 import os
+import logging
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 application = get_wsgi_application()
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as exc:
+    logging.getLogger(__name__).error(f"Startup migration: {exc}")
